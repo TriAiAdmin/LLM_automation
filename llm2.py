6acf233bfbed1fe46f2cf90a2382b936a12b9f4c
gobigweb <sameeramadushan0812@gmail.com>
2024-04-09 21:52:52 +0530

    fix prompt

--- llm2.py
+++ llm2.py
@@ -82,7 +82,7 @@ def get_openai_response(base64_image,prompt):
         response_text = raw_json.replace("json", "").replace("```", "").strip()
         try:
             response_data = json.loads(response_text)
-            company_code = response_data.get('company_code')
+            sbu = response_data.get('sbu')
             invoice_type = response_data.get('invoice_type')
             invoice_no = response_data.get('invoice_no')
             po_number = response_data.get('po_number')
@@ -91,8 +91,8 @@ def get_openai_response(base64_image,prompt):
             invoice_tax_amount = response_data.get('invoice_tax_amount')
             comment = response_data.get('comment')
         except json.JSONDecodeError:
-            company_code, invoice_type, invoice_no, po_number, invoice_date, invoice_amount, invoice_tax_amount, comment  = None, None, None, None, None, None, None, None
-        return company_code, invoice_type, invoice_no, po_number, invoice_date, invoice_amount, invoice_tax_amount, comment
+            sbu, invoice_type, invoice_no, po_number, invoice_date, invoice_amount, invoice_tax_amount, comment  = None, None, None, None, None, None, None, None
+        return sbu, invoice_type, invoice_no, po_number, invoice_date, invoice_amount, invoice_tax_amount, comment
     else:
         print(f"Error in OpenAI API response: {response.status_code} - {response.text}")
         return None, None
@@ -108,7 +108,7 @@ def find_best_match(name, address, vendor_df):
 
 def process_files(batch_files):
     prompt = '''Extract the following details from the invoice:
-        Company Code
+        sbu
         Invoice Type (Non-Tax Invoices, Tax Invoices, or SVAT Invoices),
         Invoice No,
         PO number,
@@ -116,7 +116,7 @@ def process_files(batch_files):
         Invoice Amount,
         Invoice Tax Amount,
         from the image and return the response as a JSON object with
-        'company_code'
+        'sbu'
         'invoice_type',
         'invoice_no',
         'po_number',
@@ -159,31 +159,37 @@ def process_files(batch_files):
 
     When considering the po number should follow below details:
 
-    1. If there is any value is missing for PO number name, Do not match with the address when there is no po number.set it to null only.
-    2. If there is a PO number or in other alternative name it should be 10 digit and if it less than 10 add 0 before it till it become 10 digit.
-    3. When PO number name is not mentioned consider these always alternative name for PO number: purchase order number , order number, buyer order number, your order refrence number, PO NO, Customer PO, Cust. PO No, Purchase Ord.No , Purchase and Manual NO.
-    4. Extract the exact numbers in the invoices which is relevent to the given name.
-
-    5. When PO number first digit is not clear have to match with the address which is belowed here and arrange the first digit.  
-    as per the given range first digit.
-    6. Ranges Should be:
+    1. If there is any value is missing for PO name, Do not match with the address when there is no po number.set it to null only, ensure this all the time, make sure always if PO number is not there do not match it with the address, set it to null only.
+    2. When PO number or alternative names for PO number's number count is less then 8 numbers it should set to Wrong PO, should display in jason as Wrong PO it's must follow this, ensure this always. 
+    3. If there is a PO number or in other alternative name it should be 10 digit and if it 9 digit add 0 before and make it 10 digit.
+    4. If PO number or it's alternative names are less then 9 digits it should display as wrong PO.
+    5. When PO number name is not mentioned consider these always alternative name for PO number: purchase order number , order number, buyer order number, your order refrence number, PO NO, Customer PO, Cust. PO No, Purchase Ord.No , Purchase, company name Order No and Manual NO.
+    6. Extract the exact numbers in the invoices which is relevent to the given name. Ensure all the time to extract the correct number.
+    7. When PO number first digit is not clear have to match with the address which is belowed here and arrange the first digit.  
+       as per the given range first digit make sure always this rule is for only you found PO Number.
+    8. Make sure all the time if PO Name or Other Alternative names doesn't mention in the upload invoice it should be set to null always, make sure always if PO number is not there do not match it with the address, this is must , ensure this output always. 
+    9. Ensure all the time to extract the PO numbers correctly same as in the uploaded invoice.
+    10. Always check if the PO number or other alternatives do not meet the criteria in the preferred ranges, and if the PO number or alternative name is less than 9 digits, it should be set as the wrong PO.
+        You should not compare it with the address when there is no PO number or the other alternative name is not there. The sbu should be set to null, make ensure this always.
+    11. When PO number or alternative names for PO number is null or Wrong PO make sure sbu also null.
+    12. Ranges Should be:
         - If po number between 7100000000 - 7999999999 address will be Ceylon Buiscuit Limited, Makumbura Pannipitiya.
-            It's 'company_code' will be C100.
+            It's 'sbu' will be C100.
         - If po number between 0041000000 - 0051999999 address will be CBL Food International (PVT) Limited, Ranala.
-            It's 'company_code' will be C200.
+            It's 'sbu' will be C200.
         - If po number between 0310000001 - 0389999999 address will be Convenience food (PVT) LTD,7th Lane,Off Borupana Road,Kandawala,Ratmalana.
-            It's 'company_code' will be C300.
+            It's 'sbu' will be C300.
         - If po number between 0400000000 - 0469999999 address will be CBL Plenty foods (PVT) LTD,Sir John Kothalawala Mawatha,Ratmalana.
-            It's 'company_code' will be C400.
+            It's 'sbu' will be C400.
         - If po number between 5300000000 - 5999999999 address will be CBL Exports (PVT) LTD,Seethawaka Export Processing Zone,Seethawaka.
-            It's 'company_code' will be C500.
+            It's 'sbu' will be C500.
         - If po number between 0610000000 - 0659999999 address will be CBL Natural Foods (PVT) LTD,Awariwatte Road, Heenetiyana,Minuwangoda.
-            It's 'company_code' will be C600.
+            It's 'sbu' will be C600.
         - If po number between 1710000001 - 1759999999 address will be CBL Cocos (PVT) LTD,No. 145, Colombo Rd, Alawwa, Alawwa.
-            It's 'company_code' will be C700.
+            It's 'sbu' will be C700.
         - If po number between 1810000001 - 1869999999 address will be CBL Global Foods (PVT) LTD,Colombo Road, Alawwa.
-            It's 'company_code' will be C700.
-    7. When PO number not in above range it should identify as a wrong PO.
+            It's 'sbu' will be C800.
+    13. Ensure the above structure thoroughly it's essential always.
 
     Ensure that all currency values are converted to a standard format ISO 4217, default value LKR, also base on supplier address and supplier phone number.
 
@@ -210,11 +216,11 @@ def process_files(batch_files):
 
         for image in images:
             base64_image = encode_image_to_base64(image)
-            company_code, invoice_type, invoice_no, po_number, invoice_date, invoice_amount, invoice_tax_amount, comment = get_openai_response(base64_image,prompt)
-            if company_code or invoice_type or invoice_no or po_number  or invoice_date or invoice_amount or invoice_tax_amount:
+            sbu, invoice_type, invoice_no, po_number, invoice_date, invoice_amount, invoice_tax_amount, comment = get_openai_response(base64_image,prompt)
+            if sbu or invoice_type or invoice_no or po_number  or invoice_date or invoice_amount or invoice_tax_amount:
             
                 desired_output = {
-                    "company_code": company_code,
+                    "sbu": sbu,
                     "invoice_type": invoice_type,
                     "invoice_no": invoice_no,
                     "po_number": po_number,
